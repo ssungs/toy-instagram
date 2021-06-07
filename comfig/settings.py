51f8abadddf45bf1a8c60e863480e474ae124a8c
ssungs <asdf4651@naver.com>
2021-06-07 21:42:56 +0900

    feat: Upload image
    add code
    
    - views.py
    - services.py
    - urls.py
    - models.py
    
    close: #7

--- comfig/settings.py
+++ comfig/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os, json
 from django.core.exceptions import ImproperlyConfigured
+from django.conf import settings
+from django.conf.urls.static import static
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -139,6 +141,9 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/image/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+urlpatterns = []
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
